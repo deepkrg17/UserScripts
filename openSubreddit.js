49a40d78fdf133c11d5b0f23f4f953999f798b1e
Deep Kr. Ghosh <75083264+deepkrg17@users.noreply.github.com>
2024-01-09 08:17:26 +0530

    Update openSubreddit patterns

--- openSubreddit.js
+++ openSubreddit.js
@@ -1,18 +1,20 @@
 // ==UserScript==
 // @name         Open Subreddit
 // @author       deepkrg17
-// @source       https://github.com/deepkrg17/UserScripts
+// @source       https://github.com/deepkrg17/UserScripts/raw/main/openSubreddit.js
 // @description  Redirect to reddit if searching a subreddit name
 // @match        https://www.google.com/search?*
 // @match        https://duckduckgo.com/?*
-// @version      1.0
+// @version      1.1
 // @run-at       document-start
 // ==/UserScript==
 
 const urlParams = new URLSearchParams(window.location.search);
 const query = urlParams.get("q")
-const subreddit_pattern = /^r\/[a-z_]+$/i
 
-if (subreddit_pattern.test(query)) {
+const subredditPat = /^r\/[A-Za-z0-9]\w{2,20}$/
+const userPat = /^(u|user)\/[\w-]{3,20}$/
+
+if (subredditPat.test(query) || userPat.test(query) ) {
     window.location.replace("https://www.reddit.com/" + query)
 }
